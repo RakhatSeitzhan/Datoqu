992870db5595fa53682feba175fbf97da338efb0
Seitzhan Rakhat <r.seitzhan.r@gmail.com>
2024-03-10 02:20:16 +0900

    init

---
+++ datoqu/app/login/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField'
+import Divider from '@mui/material/Divider';
+
+function Login() {
+  return (
+    <section>
+        <form className="flex-col mx-auto">
+asdf
+          <TextField variant="standard" placeholder='Name' className=""></TextField>
+          <Button variant="outlined">Log in</Button>
+        </form>
+    </section>
+
+  )
+}
+
+export default Login
---
+++ datoqu/firebase.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACpQmyGyVjnfMsI4gPI4LH1YyYh08E4BY",
+  authDomain: "datoqu-4b3b9.firebaseapp.com",
+  projectId: "datoqu-4b3b9",
+  storageBucket: "datoqu-4b3b9.appspot.com",
+  messagingSenderId: "328352547735",
+  appId: "1:328352547735:web:237651a5f0e8697217fad2",
+  measurementId: "G-E9Y8X9EMK2"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db }
